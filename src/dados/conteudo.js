67e8148524f23587e354494403d6aa5fd9ff7691
Juliana Negreiros <ju_bsnegreiros@hotmail.com>
2020-07-26 15:15:59 -0300

    Update conteudo.js

--- src/dados/conteudo.js
+++ src/dados/conteudo.js
@@ -33,4 +33,15 @@ export default [
       'música',
     ],
   },
+  {
+    nome: 'Mario Souto',
+    foto: 'https://avatars1.githubusercontent.com/u/13791385?s=460&u=6f629f0aa7530d62552d7b746fc106531588f638&v=4',
+    nomeDoProjeto: 'DevSoutinhoFlix',
+    url: 'https://www.netflix.com/',
+    tags: [
+      'front end',
+      'filmes',
+      'música',
+    ],
+  },
 ];
