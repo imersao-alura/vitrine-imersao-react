e078457557430ec78c0d73e1338aaa5f65c711b0
Gabriel Lisboa Barbosa <63424657+barbosajr10@users.noreply.github.com>
2020-07-27 23:28:32 -0300

    Update conteudo.js

--- src/dados/conteudo.js
+++ src/dados/conteudo.js
@@ -1,36 +1,13 @@
 export default [
   {
-    nome: 'Marco Bruno',
-    foto: 'https://avatars2.githubusercontent.com/u/4096456?s=400&v=4',
-    nomeDoProjeto: 'Marco Bruno Flix',
-    url: 'https://twitter.com/marcobrunodev/',
+    nome: 'Gabriel Barbosa',
+    foto: 'https://avatars3.githubusercontent.com/u/63424657?s=460&u=89e0f0bb261d3c37ba7f0861e72c9b3104a4acc8&v=4',
+    nomeDoProjeto: 'barbosaflix',
+    url: 'https://www.linkedin.com/in/gabriel-barbosa-951017/',
     tags: [
-      'front end',
+      'programação',
       'filmes',
-      'música',
-    ],
-  },
-  {
-    nome: 'Juliana Negreiros',
-    redeSocial: 'https://twitter.com/juunegreiros',
-    foto: 'https://avatars0.githubusercontent.com/u/17054729?s=460&u=7f84a845859479f59c18063284260644928f094c&v=4',
-    nomeDoProjeto: 'Juflix',
-    url: 'https://www.alura.com.br/imersao-react',
-    tags: [
-      'front end',
-      'filmes',
-      'música',
-    ],
-  },
-  {
-    nome: 'Mario Souto',
-    foto: 'https://avatars1.githubusercontent.com/u/13791385?s=460&u=6f629f0aa7530d62552d7b746fc106531588f638&v=4',
-    nomeDoProjeto: 'DevSoutinhoFlix',
-    url: 'https://www.netflix.com/',
-    tags: [
-      'front end',
-      'filmes',
-      'música',
+      'séries',
     ],
   },
 ];
