49de8dfba92b0fae7bbc165bb5a99786988e94a6
Nathaly Mesquita <nathy-mesquita@users.noreply.github.com>
2020-07-30 20:54:14 -0300

    Update conteudo.js

--- src/dados/conteudo.js
+++ src/dados/conteudo.js
@@ -299,4 +299,18 @@ export default [
       'séries',
     ],
   },
+  {
+    nome: 'Nathaly Mesquita',
+    foto: 'https://avatars0.githubusercontent.com/u/19518771?s=460&u=d7d3f2a6399376e71e2279bfc6599ce47446deb6&v=4',
+    nomeDoProjeto: 'Nathyflix',
+    url: 'https://nathyflix.vercel.app/',
+    tags: [
+      'investimentos',
+      'tecnologia',
+      'empreendedorismo',
+      'concurso',
+      'viagens',
+      'yoga',
+    ],
+  },
 ];
