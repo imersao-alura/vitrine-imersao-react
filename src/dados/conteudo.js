e74b843172b398c55255d4b12f357a03fe156465
Heliomar P. Marques <heliomarpm@gmail.com>
2020-07-31 14:27:07 -0300

    Adicionado por Heliomar Marques

--- src/dados/conteudo.js
+++ src/dados/conteudo.js
@@ -299,4 +299,13 @@ export default [
       'séries',
     ],
   },
+  {
+    nome: 'Heliomar Marques',
+    foto: 'https://avatars0.githubusercontent.com/u/13087389?s=460&v=4',
+    nomeDoProjeto: 'BMX Flix',
+    url: 'https://bmxflix.vercel.app/',
+    tags: [
+      'esporte','bmx','racing','bike','bicicleta','cross'
+    ],
+  },
 ];
