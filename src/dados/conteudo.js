3fb7f40f28295a64fb2f950e5df54dd3072affd5
Francisco Rodrigues <36885950+rodriguesfrancisco@users.noreply.github.com>
2020-07-31 16:31:39 -0300

    Altera url do site

--- src/dados/conteudo.js
+++ src/dados/conteudo.js
@@ -325,7 +325,7 @@ export default [
     nome: 'Francisco Rodrigues',
     foto: 'https://avatars2.githubusercontent.com/u/36885950?s=460&u=37b10a9355b3ce3c912d2e75f5efe8fb57b0ab8d&v=4',
     nomeDoProjeto: 'InvestFlix',
-    url: 'https://lolflix.vercel.app/',
+    url: 'https://investflix-git-master.rodriguesfrancisco.vercel.app/',
     tags: [
       'investimentos',
       'negócios',
