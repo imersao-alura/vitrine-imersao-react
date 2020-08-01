8ad1bda47ac7c9e45c0fcc19189a8fe5c41424f9
Emerson Moreira <42968718+eemr3@users.noreply.github.com>
2020-08-01 10:43:56 -0300

    Update conteudo.js

--- src/dados/conteudo.js
+++ src/dados/conteudo.js
@@ -4,7 +4,11 @@ export default [
     foto: 'https://avatars2.githubusercontent.com/u/4096456?s=400&v=4',
     nomeDoProjeto: 'Marco Bruno Flix',
     url: 'https://twitter.com/marcobrunodev/',
-    tags: ['front end', 'filmes', 'música'],
+    tags: [
+      'front end', 
+      'filmes', 
+      'música'
+    ],
   },
   {
     nome: 'Juliana Negreiros',
@@ -13,7 +17,11 @@ export default [
       'https://avatars0.githubusercontent.com/u/17054729?s=460&u=7f84a845859479f59c18063284260644928f094c&v=4',
     nomeDoProjeto: 'Juflix',
     url: 'https://www.alura.com.br/imersao-react',
-    tags: ['front end', 'filmes', 'música'],
+    tags: [
+      'front end', 
+      'filmes', 
+      'música'
+    ],
   },
   {
     nome: 'Mario Souto',
@@ -21,13 +29,21 @@ export default [
       'https://avatars1.githubusercontent.com/u/13791385?s=460&u=6f629f0aa7530d62552d7b746fc106531588f638&v=4',
     nomeDoProjeto: 'DevSoutinhoFlix',
     url: 'https://www.netflix.com/',
-    tags: ['front end', 'filmes', 'música'],
+    tags: [
+      'front end', 
+      'filmes', 
+      'música'
+    ],
   },
   {
     nome: 'Emerson Moreira',
     foto: 'https://avatars2.githubusercontent.com/u/42968718?s=460&v=4',
     nomeDoProjeto: 'EmerFlix',
     url: 'https://emerflix.vercel.app/',
-    tags: ['front end', 'Back end', 'Games'],
+    tags: [
+      'front end', 
+      'Back end', 
+      'Games'
+    ],
   },
 ]
