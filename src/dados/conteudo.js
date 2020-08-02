7bfdbbd43299235473469e86bc806d3ef7c772f2
Gabriel Ronei <gabriheron@hotmail.com>
2020-08-02 16:55:36 -0300

    Atualizando infos! :)

--- src/dados/conteudo.js
+++ src/dados/conteudo.js
@@ -661,4 +661,17 @@ export default [
       'curiosidades',
     ],
   },
+  {
+    nome: 'Gabriel Ronei de Oliveira Paulo',
+    foto: 'http://1.gravatar.com/avatar/64e08c3e7e5eef2745e087b3f379adbc',
+    nomeDoProjeto: 'MUSICHUB',
+    url: 'https://musichub.vercel.app/',
+    tags: [
+      'músicas',
+      'metal',
+      'rap',
+      'folk',
+      'curiosidades',
+    ],
+  },
 ];
