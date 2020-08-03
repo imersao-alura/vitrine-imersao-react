4a9adac6ca06c368b3fe24fa0d17905dc480de30
Juliane Pires <36718319+JulianePires@users.noreply.github.com>
2020-08-03 09:52:39 -0300

    Update conteudo.js

--- src/dados/conteudo.js
+++ src/dados/conteudo.js
@@ -1089,4 +1089,32 @@ export default [
       'Games',
     ],
   },
+  {
+    nome: 'Leandro Almeida',
+    foto: 'https://avatars2.githubusercontent.com/u/30052919?s=460&u=e7f2efe21341d9159a6dc944d1b795f9bd04b807&v=4',
+    nomeDoProjeto: 'garoupicks',
+    url: 'https://garoupicks.vercel.app/',
+    tags: [
+      'Filmes',
+      'Ficção científica',
+      'Terror',
+      'Games',
+    ],
+  },
+  {
+    nome: 'Juliane Pires',
+    foto: 'https://avatars1.githubusercontent.com/u/36718319?s=460&u=a013252bab4b605e42d542498617a5eecc1ed250&v=4',
+    nomeDoProjeto: 'Miojoflix',
+    url: 'https://miojoflix.netlify.app',
+    tags: [
+      'Front End',
+      'Aprendendo Tecnologia',
+      'Música',
+      'Games',
+      'Conversas e Entrevistas',
+      'Tutoriais',
+      'Cozinha',
+      'Ted Talks',
+    ],
+  },
 ];
