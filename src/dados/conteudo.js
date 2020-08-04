82a9660f379d509211aeff61b01f03260cdff364
Daractor <48837388+daractor@users.noreply.github.com>
2020-08-04 15:03:14 -0300

    Update conteudo.js

--- src/dados/conteudo.js
+++ src/dados/conteudo.js
@@ -1180,4 +1180,20 @@ export default [
       'vida saudável',
     ],
   },
+  {
+    nome: 'Daractor',
+    foto: 'https://avatars0.githubusercontent.com/u/48837388?s=460&v=4',
+    nomeDoProjeto: 'techflix',
+    url: 'https://techflix-roan.vercel.app/',
+    tags: [
+      'Tecnologia',
+      'Back End',
+      'Front End',
+      'Data Science',
+      'Desenvolvimento',
+      'Filmes',
+      'Games',
+      'Músicas'
+    ],
+  },
 ];
